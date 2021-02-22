921cc1a8f3bee73d67e8ecc6646d804736760a5b
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-02-22 21:16:01 +0530

    Update VideoHeader.js

--- VideoHeader.js
+++ VideoHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './VideoHeader.css'
+import './VideoHeader.css';
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 import CameraAltOutlinedIcon from "@material-ui/icons/CameraAltOutlined";
